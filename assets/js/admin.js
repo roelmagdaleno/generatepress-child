4a161ebb074943df32f31d40644eed374d26ad22
Roel Magdaleno <roelmagdaleno@gmail.com>
2019-11-10 15:41:32 -0600

    FEATURE: Add a new result row with JS added

---
+++ assets/js/admin.js
@@ -0,0 +1,19 @@
+/**
+ * Add a new row in the results table.
+ *
+ * We will allow the user to add a new results
+ * to the frontend.
+ *
+ * @since 0.1.0
+ */
+function RMR_addResult() {
+    let resultsBodyEl = document.getElementById( 'rmr-results-tbody' );
+    let metaboxId     = resultsBodyEl.dataset.metaboxId;
+
+    let rowEl = '<tr>';
+    rowEl    += `<td> <input type="text" name="${metaboxId}[title][]" class="large-text widefat" /> </td>`;
+    rowEl    += `<td> <textarea name="${metaboxId}[content][]" class="large-text widefat"></textarea> </td>`;
+    rowEl    += '</tr>';
+
+    resultsBodyEl.innerHTML += rowEl;
+}
