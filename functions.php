54f4888583a3477fd15bdf5bc18ea35d495869c6
Roel Magdaleno <roelmagdaleno@gmail.com>
2019-11-10 15:39:10 -0600

    FEATURE: Enqueue admin scripts on post.php pages

--- functions.php
+++ functions.php
@@ -6,4 +6,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once 'includes/required-files.php';
 
+/**
+ * Enqueue the admin scripts only in the "post.php"
+ * admin page.
+ *
+ * @since 0.1.0
+ *
+ * @param string   $hook   	The current admin page.
+ */
+function rmr_enqueue_scripts( $hook ) {
+	if ( 'post.php' !== $hook ) {
+		return;
+	}
+
+	$assets_path = get_stylesheet_directory_uri() . '/assets/';
+
+	wp_enqueue_script(
+		'rmr-admin-assets',
+		$assets_path . 'js/admin.js',
+		array(),
+		RMR_VERSION,
+		true
+	);
+}
+add_action( 'admin_enqueue_scripts', 'rmr_enqueue_scripts' );
+
 new RMR_Post_Types();
