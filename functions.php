1acfbce36d2bab4b472223a6354c0c6092e7540a
Roel Magdaleno <roelmagdaleno@gmail.com>
2020-06-23 21:25:47 -0500

    FEATURE: Add local fonts in Gutenberg editor

--- functions.php
+++ functions.php
@@ -6,9 +6,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once 'includes/constants.php';
 
+add_action( 'enqueue_block_editor_assets', 'rmr_load_fonts_in_gutenberg_editor' );
 add_action( 'wp_enqueue_scripts', 'rmr_load_custom_code_fonts' );
 add_filter( 'generate_logo_attributes', 'rmr_lazy_logo', 10, 1 );
 add_filter( 'generate_typography_default_fonts', 'rmr_load_local_fonts', 10, 1 );
+
+/**
+ * Load ASAP fonts in Gutenberg editor.
+ *
+ * Why we have to do this?
+ * Gutenberg won't load the local fonts we just added. Previously was
+ * adding the fonts from Google Fonts.
+ *
+ * @since 0.1.0
+ */
+function rmr_load_fonts_in_gutenberg_editor() {
+	wp_enqueue_style( 'rmr-styles', get_stylesheet_uri() );
+}
 
 /**
  * Load the "JetBrains Mono" font in single posts and only
