04ee46c5392ac04e0cb1332b331ea2e97e4701c4
Roel Magdaleno <roelmagdaleno@gmail.com>
2020-06-23 20:32:07 -0500

    FEATURE: Add screenshot image

--- includes/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
-// The current theme version.
-define( 'RMR_VERSION', '0.1.0' );
--- includes/required-files.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
-require_once 'constants.php';
-require_once 'helpers/render-components.php';
-require_once 'class-rmr-post-types.php';
