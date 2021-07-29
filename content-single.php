1d9c4b6caf9fd559923134c6a806771273e06735
Roel Magdaleno <roelmagdaleno@gmail.com>
2021-07-28 22:12:03 -0500

    FEATURE: Add author bio box at the end of the post

---
+++ content-single.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * The template for displaying single posts.
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_do_microdata( 'article' ); ?>>
+	<div class="inside-article">
+		<?php
+		/**
+		 * generate_before_content hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_featured_page_header_inside_single - 10
+		 */
+		do_action( 'generate_before_content' );
+
+		if ( generate_show_entry_header() ) :
+			?>
+			<header class="entry-header">
+				<?php
+				/**
+				 * generate_before_entry_title hook.
+				 *
+				 * @since 0.1
+				 */
+				do_action( 'generate_before_entry_title' );
+
+				if ( generate_show_title() ) {
+					$params = generate_get_the_title_parameters();
+
+					the_title( $params['before'], $params['after'] );
+				}
+
+				/**
+				 * generate_after_entry_title hook.
+				 *
+				 * @since 0.1
+				 *
+				 * @hooked generate_post_meta - 10
+				 */
+				do_action( 'generate_after_entry_title' );
+				?>
+			</header>
+		<?php
+		endif;
+
+		/**
+		 * generate_after_entry_header hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_post_image - 10
+		 */
+		do_action( 'generate_after_entry_header' );
+
+		$itemprop = '';
+
+		if ( 'microdata' === generate_get_schema_type() ) {
+			$itemprop = ' itemprop="text"';
+		}
+		?>
+
+		<div class="entry-content"<?php echo $itemprop; // phpcs:ignore -- No escaping needed. ?>>
+			<?php
+			the_content();
+
+			?>
+
+			<div class="rmr-author-bio">
+				<div class="rmr-author-bio__avatar">
+					<img src="https://cdn.roelmagdaleno.com/assets/2019/08/ZHFS36po_400x400.jpg" alt="Roel Magdaleno">
+				</div>
+				<div class="rmr-author-bio__bio">
+					<strong class="rmr-author-bio__written-by">
+						Escrito por <?php echo get_the_author_meta( 'display_name' ); ?>
+					</strong>
+					<p><?php echo get_the_author_meta( 'description' ); ?></p>
+				</div>
+			</div>
+
+			<?php
+
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'generatepress' ),
+					'after'  => '</div>',
+				)
+			);
+			?>
+		</div>
+
+		<?php
+		/**
+		 * generate_after_entry_content hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_footer_meta - 10
+		 */
+		do_action( 'generate_after_entry_content' );
+
+		/**
+		 * generate_after_content hook.
+		 *
+		 * @since 0.1
+		 */
+		do_action( 'generate_after_content' );
+		?>
+	</div>
+</article>
