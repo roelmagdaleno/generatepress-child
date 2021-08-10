2b367e51c25c62b38865453f8020e67884eed6a5
Roel Magdaleno <roelmagdaleno@gmail.com>
2021-08-09 19:11:16 -0500

    FEATURE: Post content images as webp

--- includes/images.php
+++ includes/images.php
@@ -5,6 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 add_filter( 'post_thumbnail_html', 'rmr_featured_images_as_webp' );
+add_filter( 'the_content', 'rmr_images_as_webp' );
 
 /**
  * Use featured image as webp instead of png, jpg or jpeg.
@@ -22,4 +23,22 @@ function rmr_featured_images_as_webp( $html ) {
 	);
 
 	return str_replace( $extensions, '.webp', $html );
+}
+
+/**
+ * Use images as webp instead of png, jpg or jpeg for post content.
+ *
+ * @since  0.1.8
+ *
+ * @param  string   $content   The post content.
+ * @return string              The post content with images as webp.
+ */
+function rmr_images_as_webp( $content ) {
+	$extensions = array(
+		'.png',
+		'.jpg',
+		'.jpeg',
+	);
+
+	return str_replace( $extensions, '.webp', $content );
 }
