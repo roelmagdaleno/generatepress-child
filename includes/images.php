27c18652c6179fc67809084edf4138c1c45bf491
Roel Magdaleno <roelmagdaleno@gmail.com>
2021-08-22 18:18:21 -0500

    IMPROVE: Remove unnecessary S3Uploads function

--- includes/images.php
+++ includes/images.php
@@ -9,18 +9,6 @@ add_filter( 'the_content', 'rmr_images_as_webp' );
 add_action( 'admin_init', 'rmr_disable_s3_path_on_admin' );
 
 /**
- * Disable the S3 bucket link for uploads directory only in admin side.
- *
- * Roel do this because the upload.php dashboard loads very slow when
- * loading the images from the CDN. Instead, load the local links for every image.
- *
- * @since 0.1.9
- */
-function rmr_disable_s3_path_on_admin() {
-	define( 'S3_UPLOADS_DISABLE_REPLACE_UPLOAD_URL', true );
-}
-
-/**
  * Use featured image as webp instead of png, jpg or jpeg.
  *
  * @since  0.1.8
