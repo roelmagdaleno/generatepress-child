9c6b34e1fa741f016c6d998c11923a0d3211b047
Roel Magdaleno <roelmagdaleno@gmail.com>
2021-10-01 13:59:56 -0500

    FEATURE: Use Poppins font instead of Inter

--- includes/fonts.php
+++ includes/fonts.php
@@ -14,12 +14,18 @@ add_filter( 'generate_typography_default_fonts', 'rmr_load_local_fonts', 10, 1 )
  * @since 0.1.1   Preload JetBrains Mono font.
  */
 function rmr_preload_fonts() {
-	echo '<link rel="preload" href="' . RMR_THEME_URI . '/assets/fonts/inter/Inter-Regular.woff2" as="font" type="font/woff2" crossorigin>';
+	$fonts = array(
+		'poppins-v15-latin-600.woff2',
+		'poppins-v15-latin-700.woff2',
+		'poppins-v15-latin-regular.woff2',
+	);
 
-	echo '<link rel="preload" href="' . RMR_THEME_URI . '/assets/fonts/inter/Inter-Bold.woff2" as="font" type="font/woff2" crossorigin>';
+	foreach ( $fonts as $font ) {
+		echo rmr_get_font_link( 'poppins', $font );
+	}
 
 	if ( is_single() && has_block( 'core/code' ) ) {
-		echo '<link rel="preload" href="' . RMR_THEME_URI . '/assets/fonts/jetbrains-mono/JetBrainsMono-Regular.woff2" as="font" type="font/woff2" crossorigin>';
+		echo rmr_get_font_link( 'jetbrains-mono', 'JetBrainsMono-Regular.woff2' );
 	}
 }
 
@@ -35,9 +41,25 @@ function rmr_preload_fonts() {
  * @param  array   $fonts   The current default fonts.
  * @return array            The fonts with our custom ones.
  */
-function rmr_load_local_fonts( $fonts ) {
-	$fonts[] = 'Inter';
+function rmr_load_local_fonts( array $fonts ) : array {
+	$fonts[] = 'Poppins';
 	$fonts[] = 'JetBrains Mono';
 
 	return $fonts;
+}
+
+/**
+ * Get a font <link> to print in <head> tag.
+ *
+ * @since  0.2.4
+ *
+ * @param  string   $font_family   Current font family.
+ * @param  string   $font          The font to be loaded.
+ * @return string                  The font <link>.
+ */
+function rmr_get_font_link( string $font_family, string $font ) : string {
+	$link  = '<link rel="preload" href="' . RMR_THEME_URI . '/assets/fonts/' . $font_family . '/' . $font . '" ';
+	$link .= 'as="font" type="font/woff2" crossorigin>';
+
+	return $link;
 }
