7edcc0a484ffa9c221da1b3b1ef38ef845f57fb1
Roel Magdaleno <roelmagdaleno@gmail.com>
2021-11-08 21:57:51 -0600

    FEATURE: Share buttons widget in own theme

--- includes/widgets.php
+++ includes/widgets.php
@@ -3,6 +3,13 @@
 require_once 'widgets/share.php';
 
 add_action( 'widgets_init', 'rmr_register_widgets' );
+
+/**
+ * Register custom widgets for https://roelmagdaleno.com
+ * Use an array of widget names to loop it using foreach or array_map.
+ *
+ * @since 0.2.7
+ */
 function rmr_register_widgets() {
 	$widgets = array(
 		'RMR_Share',
