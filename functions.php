c61ce07bf696cd6b168ececf2f80e98ad5384742
Roel Magdaleno <roelmagdaleno@gmail.com>
2022-02-19 16:04:44 -0600

    IMPROVE: Remove dark mode

--- functions.php
+++ functions.php
@@ -27,7 +27,6 @@ remove_action( 'wp_head', 'feed_links', 2 );
 add_action( 'wp_head', 'feed_links' );
 add_action( 'wp_enqueue_scripts', 'rmr_load_custom_assets' );
 add_action( 'enqueue_block_editor_assets', 'rmr_load_fonts_in_gutenberg_editor' );
-add_action( 'generate_menu_bar_items', 'rmr_add_dark_mode_button', 20 );
 add_action( 'rmr_generatepress_after_site_content', 'generate_do_comments_template', 15 );
 add_action( 'init', 'rmr_disable_emojis' );
 add_action( 'wp_loaded', 'rmr_wp_loaded' );
@@ -56,21 +55,6 @@ function rmr_get_estimated_reading_time( string $content = '', int $wpm = 250 ) 
  */
 function rmr_wp_loaded() {
 	remove_action( 'generate_after_do_template_part', 'generate_do_comments_template', 15 );
-}
-
-/**
- * Add the dark mode button into the header navigation.
- * @since 0.1.2
- */
-function rmr_add_dark_mode_button() {
-	$moon = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>';
-
-	$sun = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>';
-
-	echo '<button type="button" aria-label="Toggle Dark Mode" class="rmr__dark-mode__btn" onclick="window.rmr.darkMode.add()">';
-	echo $moon;
-	echo $sun;
-	echo '</button>';
 }
 
 /**
@@ -115,21 +99,6 @@ function rmr_load_custom_assets() {
 	wp_deregister_script( 'wp-embed' );
 
 	$uri = get_stylesheet_directory_uri();
-
-	wp_enqueue_style(
-		'rmr-dark-mode-styles',
-		$uri . '/assets/css/dark-mode.css',
-		null,
-		RMR_VERSION
-	);
-
-	wp_enqueue_script(
-		'rmr-dark-mode',
-		$uri . '/assets/js/dark-mode.js',
-		null,
-		RMR_VERSION,
-		true
-	);
 
 	if ( 'hub' === get_post_type() ) {
 		wp_enqueue_style(
