b049b1ae9caaac56aee1626c4c72676e75141c66
Roel Magdaleno <roelmagdaleno@gmail.com>
2022-02-20 00:20:21 -0600

    IMPROVE: Remove lazy load from featured images

--- includes/images.php
+++ includes/images.php
@@ -5,7 +5,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 add_filter( 'post_thumbnail_html', 'rmr_featured_images_as_webp' );
+add_filter( 'generate_single_featured_image_output', 'rmr_remove_featured_image_lazy_load' );
 add_filter( 'the_content', 'rmr_images_as_webp' );
+
+/**
+ * Remove lazy load from post featued image.
+ *
+ * @since  0.3.0
+ *
+ * @param  string   $html   The post featured image.
+ * @return string           The post featured image without lazy load.
+ */
+function rmr_remove_featured_image_lazy_load( string $html ) : string {
+	return str_replace( 'loading="lazy"', '', $html );
+}
 
 /**
  * Use featured image as webp instead of png, jpg or jpeg.
@@ -15,7 +28,7 @@ add_filter( 'the_content', 'rmr_images_as_webp' );
  * @param  string   $html   The post thumbnail HTML.
  * @return string           The post thumbnail HTML as webp.
  */
-function rmr_featured_images_as_webp( $html ) {
+function rmr_featured_images_as_webp( string $html ) : string {
 	$extensions = array(
 		'.png',
 		'.jpg',
