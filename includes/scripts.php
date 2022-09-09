adf750800ee64f647290dbb74d4a563a74e7773c
Roel Magdaleno <roelmagdaleno@gmail.com>
2022-09-09 11:54:31 -0500

    Restore Gutenberg CSS

--- includes/scripts.php
+++ includes/scripts.php
@@ -5,18 +5,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 add_action( 'wp_enqueue_scripts', 'rmr_load_custom_assets' );
-add_action( 'wp_enqueue_scripts', 'rmr_remove_wp_block_library_css', 100 );
-
-/**
- * Do not enqueue Gutenberg block styles in frontend.
- *
- * @since  0.3.0
- */
-function rmr_remove_wp_block_library_css() {
-	wp_dequeue_style( 'wp-block-library' );
-	wp_dequeue_style( 'wp-block-library-theme' );
-	wp_dequeue_style( 'wc-blocks-style' );
-}
 
 /**
  * Load the "JetBrains Mono" font in single posts and only if the post contains
